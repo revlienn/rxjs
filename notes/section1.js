23fc94319002552f649ae2d90423239ebff1d81e
revlienn <revnathalia@gmail.com>
2023-08-03 16:01:43 +0100

    1.1-3 How to run the app+Git Init

---
+++ notes/section1.js
@@ -0,0 +1,14 @@
+/*
+git steps IF your folder hasn't been connected to other git remote
+   git init
+   git add . 
+   git commit -m "INIT"
+   git remote add origin linkHere
+   git push -u origin master
+*/
+
+/*
+npm i
+npm run Server
+npm start // frontend
+*/
